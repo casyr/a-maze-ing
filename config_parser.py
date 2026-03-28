96fb296d15e98d8a5361a85f4887e5e748f07a51
Yann Riffard <yriffard@z1r4p2.42lyon.fr>
2026-03-28 15:58:48 +0100

    adjust some but some file disapeard

--- config_parser.py
+++ config_parser.py
@@ -1,17 +1,17 @@
 class GetConfig():
 
-    def __init__(self) -> None:
+    def __init__(self, config_file_name: str) -> None:
         try:
-            with open("config.txt", "r") as f:
+            with open(config_file_name, "r") as f:
                 self.get_config_info = f.read()
         except FileNotFoundError as e:
             print("ERROR:", e)
 
     def get_data_config(self) -> dict[str, str]:
-        dict_config_info: dict[str, str] = {}
-        info_list = self.get_config_info.split("\n")
-        for x in info_list:
+        dict_config_file: dict[str, str] = {}
+        list_config_file = self.get_config_info.split("\n")
+        for x in list_config_file:
             key = x.split("=")[0]
             value = x.split("=")[1]
-            dict_config_info[key] = value
-        return (dict_config_info)
+            dict_config_file[key] = value
+        return (dict_config_file)
