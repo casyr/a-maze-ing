99ce588d644c0389cb38023342bb677284e2d8e8
Yann Riffard <yriffard@z2r2p6.42lyon.fr>
2026-03-30 13:46:42 +0200

    the parsing work and pydantic make his job . Now we had t crate the datastruct of our maze class

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
-from .config_parser import GetConfig
+from config_parser import GetConfig
 from pydantic import BaseModel, Field, ValidationError, model_validator
 from typing import Annotated
+from typing_extensions import Self
 
 
-class config_data(BaseModel):
+class ConfigData(BaseModel):
 
-    width: Annotated[int, Field(ge=100, le=1980)]
-    
+    WIDTH: Annotated[int, Field(ge=10, le=1980)]
+    HEIGHT: Annotated[int, Field(ge=10, le=1080)]
+    ENTRY_X: Annotated[int, Field(ge=0, le=1500)]
+    ENTRY_Y: Annotated[int, Field(ge=0, le=900)]
+    EXIT_X: Annotated[int, Field(ge=0, le=1500)]
+    EXIT_Y: Annotated[int, Field(ge=0, le=900)]
+    OUTPUT_FILE: Annotated[str, Field(min_length=5, max_length=50)]
+    PERFECT: bool
 
+    @model_validator(mode='after')
+    def validation(self) -> Self:
+        # add condition where start and end are in the "42"
 
-
-
-def config_checker(config: dict[str, str]):
-    ...
+        return self
 
 
 def main() -> None:
@@ -23,12 +30,16 @@ def main() -> None:
     if not len(sys.argv) == 2:
         raise Exception("You must run the programm like"
                         " <python3 a_maza_ing.py config.txt>")
-
-    config = GetConfig(sys.argv[2])
+    config_parser = GetConfig(sys.argv[1])
+    data_dict = config_parser.get_data_config()
+    data_dict["ENTRY_X"] = data_dict["ENTRY"].split(",")[0]
+    data_dict["ENTRY_Y"] = data_dict["ENTRY"].split(",")[1]
+    data_dict["EXIT_X"] = data_dict["EXIT"].split(",")[0]
+    data_dict["EXIT_Y"] = data_dict["EXIT"].split(",")[1]
     try:
-        config_checker(config.get_data_config())
-    except Exception as e:
-        print(e)
+        ConfigData.model_validate(data_dict)
+    except ValidationError as e:
+        print(f"Erreur Pydantic :\n{e}")
 
 
 if __name__ == "__main__":
