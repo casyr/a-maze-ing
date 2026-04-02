0b7c64263c3f7fa380da0860dc7f729cdab1f870
Yann Riffard <yriffard@z4r4p7.42lyon.fr>
2026-04-02 19:23:17 +0200

    solver work and the outpuy in on the apropriate file . i had to finish the maze_gen when the maze had to be perfect and also the 42 represent in the middle of the maze

--- maze_generator.py
+++ maze_generator.py
@@ -1,5 +1,6 @@
-import sys
+
 import random
+from typing import Any
 
 
 class MazeGenerator:
@@ -60,7 +61,7 @@ class MazeGenerator:
         if direction == 8:
             return 2
         return 0
-    
+
     # ##
     # @staticmethod
     # def print_maze(maze):
@@ -97,18 +98,161 @@ class MazeGenerator:
                 actual_path.append((pos_x, pos_y))
         return maze
 
-    def select_smallest_avaible(maze: list[list[int]]) -> tuple:
-        smallest = (0, 0)
+    def select_avaible_position(
+        self, maze: list[list[int]], pos_x, pos_y, visited_list: set[tuple]
+    ) -> tuple:
+        if (
+            pos_x != 0
+            and maze[pos_y][pos_x - 1] & 2 == 0
+            and (pos_x - 1, pos_y) not in visited_list
+        ):
+            return (pos_x - 1, pos_y)
+        if (
+            pos_x != self.width - 1
+            and maze[pos_y][pos_x + 1] & 8 == 0
+            and (pos_x + 1, pos_y) not in visited_list
+        ):
+            return (pos_x + 1, pos_y)
+        if (
+            pos_y != 0
+            and maze[pos_y - 1][pos_x] & 4 == 0
+            and (pos_x, pos_y - 1) not in visited_list
+        ):
+            return (pos_x, pos_y - 1)
+        if (
+            pos_y != self.height - 1
+            and maze[pos_y + 1][pos_x] & 1 == 0
+            and (pos_x, pos_y + 1) not in visited_list
+        ):
+            return (pos_x, pos_y + 1)
+        return (-1, -1)
 
-        return smallest 
-
-
-    def maze_floodfill_solver(self) -> None:
-        depth = 0
+    def create_depth_maze(self, maze: list[list[int]]) -> list[list[int]]:
+        depth_maze: list[list[int]] = self.create_full_maze(
+            self.width, self.height
+        )
         pos_x = self.exit_x
         pos_y = self.exit_y
-        
-        
+        depth = 0
+        actual_path: list[tuple] = [(pos_x, pos_y)]
+        visited_list: set[tuple] = {(pos_x, pos_y)}
+        depth_maze[pos_y][pos_x] = depth
+        while actual_path != []:
+            avaible_pos = self.select_avaible_position(
+                maze, pos_x, pos_y, visited_list
+            )
+            if avaible_pos == (-1, -1):
+                actual_path.pop()
+                if actual_path != []:
+                    pos_x, pos_y = actual_path[-1]
+                depth = depth_maze[pos_y][pos_x]
+                continue
+            pos_x, pos_y = avaible_pos
+            depth += 1
+            depth_maze[pos_y][pos_x] = depth
+            visited_list.add((pos_x, pos_y))
+            actual_path.append((pos_x, pos_y))
+        return depth_maze
 
-    def create_output_file(self, file_name) -> None:
-        pass
+    def select_min_avaible_position(
+        self,
+        maze: list[list[int]],
+        pos_x,
+        pos_y,
+        depth_maze: list[list[int]],
+        visited_list: set[tuple],
+    ) -> dict[str, Any]:
+        avaible_list: list[dict[str, Any]] = []
+        if (
+            pos_x != 0
+            and maze[pos_y][pos_x - 1] & 2 == 0
+            and not (pos_x - 1, pos_y) in visited_list
+        ):
+            avaible_list.append(
+                {
+                    "direction": "W",
+                    "pos": (pos_x - 1, pos_y),
+                    "depth": depth_maze[pos_y][pos_x - 1],
+                }
+            )
+        if (
+            pos_x != self.width - 1
+            and maze[pos_y][pos_x + 1] & 8 == 0
+            and not (pos_x + 1, pos_y) in visited_list
+        ):
+            avaible_list.append(
+                {
+                    "direction": "E",
+                    "pos": (pos_x + 1, pos_y),
+                    "depth": depth_maze[pos_y][pos_x + 1],
+                }
+            )
+        if (
+            pos_y != 0
+            and maze[pos_y - 1][pos_x] & 4 == 0
+            and not (pos_x, pos_y - 1) in visited_list
+        ):
+            avaible_list.append(
+                {
+                    "direction": "S",
+                    "pos": (pos_x, pos_y - 1),
+                    "depth": depth_maze[pos_y - 1][pos_x],
+                }
+            )
+        if (
+            pos_y != self.height - 1
+            and maze[pos_y + 1][pos_x] & 1 == 0
+            and not (pos_x, pos_y + 1) in visited_list
+        ):
+            avaible_list.append(
+                {
+                    "direction": "N",
+                    "pos": (pos_x, pos_y + 1),
+                    "depth": depth_maze[pos_y + 1][pos_x],
+                }
+            )
+        if avaible_list == []:
+            return {}
+        return min(avaible_list, key=lambda min_depth: min_depth["depth"])
+
+    def maze_floodfill_solver(self) -> list[dict]:
+        maze = self.dfs_maze_generator()
+        depth_maze = self.create_depth_maze(maze)
+        print(maze)
+        print(depth_maze)
+        pos_x = self.entry_x
+        pos_y = self.entry_y
+        solve_path: list[dict] = []
+        visited_list: set[tuple] = set([])
+        while (pos_x, pos_y) != (self.exit_x, self.exit_y):
+            min_avaible_pos = self.select_min_avaible_position(
+                maze, pos_x, pos_y, depth_maze, visited_list
+            )
+            if min_avaible_pos == {}:
+                if solve_path != []:
+                    solve_path.pop()
+                    pos_x, pos_y = solve_path[-1]
+                continue
+            pos_x, pos_y = min_avaible_pos["pos"]
+            visited_list.add((pos_x, pos_y))
+            solve_path.append(min_avaible_pos)
+        return solve_path
+
+    def maze_convertor(self) -> str:
+        maze = self.dfs_maze_generator()
+        convert_str = ""
+        for x in maze:
+            for y in x:
+                convert_str += format(y, 'x')
+            convert_str += "\n"
+        return convert_str
+
+    def create_output_file(self) -> None:
+        maze_convert = self.maze_convertor()
+        solve_path = self.maze_floodfill_solver()
+        with open(self.output_file, "w") as output_f:
+            output_f.write(f"{maze_convert}\n")
+            output_f.write(f"{self.entry_x}.{self.entry_y}\n")
+            output_f.write(f"{self.exit_x}.{self.exit_y}\n")
+            for x in solve_path:
+                output_f.write(x["direction"])
